6e8d07150d62f592ccb7285848f1082d1cd33f0b
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-10 12:20:03 +0200

    intialized fetch post to return error message

--- djs10/src/BlogPosts.js
+++ djs10/src/BlogPosts.js
@@ -9,9 +9,11 @@ const BlogPosts = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch posts');
+          throw new Error("Failed to fetch posts");
         }
         const data = await response.json();
         setPosts(data);
@@ -20,13 +22,9 @@ const BlogPosts = () => {
       }
     };
 
-    
-
-
-
-
-
-
-
-
+    fetchPosts();
+  }, []);
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 };
