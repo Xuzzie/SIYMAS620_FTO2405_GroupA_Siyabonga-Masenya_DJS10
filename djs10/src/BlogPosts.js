6ad446f612513183f65cb5087e5d82a5ba4185b8
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-10 12:03:37 +0200

    Added blog post  arrow function to deal with the fetching of information

---
+++ djs10/src/BlogPosts.js
@@ -0,0 +1,8 @@
+//First we import react into the compoenent and create an arrow function that will deal with the fetching of the user information
+
+import React, { useState, useEffect } from "react";
+
+const BlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+};
